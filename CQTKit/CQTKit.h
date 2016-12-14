31f6095efb81c75cd7657e0939056b31bd407d70
Matt Rajca <matt.rajca@me.com>
2016-12-14 10:23:54 -0800

    Initial Commit

---
+++ CQTKit/CQTKit.h
@@ -0,0 +1,19 @@
+//
+//  CQTKit.h
+//  CQTKit
+//
+//  Created by Matt on 12/14/16.
+//  Copyright © 2016 MattRajca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CQTKit.
+FOUNDATION_EXPORT double CQTKitVersionNumber;
+
+//! Project version string for CQTKit.
+FOUNDATION_EXPORT const unsigned char CQTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CQTKit/PublicHeader.h>
+
+
